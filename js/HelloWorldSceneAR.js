8fc3c1c298ca10b719af809a8eb2663ea63ff947
Slowgen <kalibobailey@gmail.com>
2020-08-21 16:07:23 -0400

    Feat: Image recognition functions with metroCard

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -10,6 +10,8 @@ import {
   ViroConstants,
   ViroBox,
   ViroMaterials,
+  ViroARTrackingTargets,
+  ViroARImageMarker
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
@@ -19,10 +21,12 @@ export default class HelloWorldSceneAR extends Component {
     // Set initial state here
     this.state = {
       text: 'Initializing AR...',
+      displaySpoon: false
     };
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
+    this._onAnchorFound = this._onAnchorFound.bind(this);
   }
 
   render() {
@@ -34,11 +38,16 @@ export default class HelloWorldSceneAR extends Component {
           position={[0, 0, -1]}
           style={styles.helloWorldTextStyle}
         />
-        <ViroBox
-          position={[0, -0.5, -1]}
-          scale={[0.3, 0.3, 0.1]}
-          materials={['grid']}
-        />
+
+        <ViroARImageMarker target={"targetSpoon"}
+          onAnchorFound={this._onAnchorFound}>
+          <ViroBox
+            position={[0, -0.5, -1]}
+            scale={[0.3, 0.3, 0.1]}
+            materials={['grid']}
+          />
+        </ViroARImageMarker>
+
       </ViroARScene>
     );
   }
@@ -46,11 +55,19 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text: 'Hello World!',
+        text: 'Find a metroCard!',
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
+      this.setState({
+        text: 'oopsie'
+      })
     }
+  }
+
+  _onAnchorFound() {
+    this.setState({
+      displaySpoon: true,
+    })
   }
 }
 
@@ -66,6 +83,14 @@ var styles = StyleSheet.create({
 ViroMaterials.createMaterials({
   grid: {
     diffuseTexture: require('./res/grid_bg.jpg'),
+  },
+});
+
+ViroARTrackingTargets.createTargets({
+  "targetSpoon" : {
+    source : require('./res/metrocard.png'),
+    orientation : "Up",
+    physicalWidth : 0.1 // real world width in meters
   },
 });
 
