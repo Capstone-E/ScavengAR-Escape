cded2a4745f156115f62052c9d08059d7629b0d9
Ashlee Pitock <ashlee.pitock@gmail.com>
2020-08-24 10:54:04 -0400

    added two portals, not currently not in view

--- js/res/ARPortals/MainScene.js
+++ js/res/ARPortals/MainScene.js
@@ -24,35 +24,62 @@ var MainScene = createReactClass({
   render: function () {
     return (
       <ViroARScene>
-        <ViroARPlaneSelector>
-          <ViroAmbientLight color="#ffffff" intensity={200} />
-          <ViroPortalScene
-            passable={true}
-            dragType="FixedDistance"
-            onDrag={() => {}}
-          >
-            <ViroPortal position={[0, 0, -1]} scale={[0.1, 0.1, 0.1]}>
-              <Viro3DObject
-                source={require('./portal_res/portal_ship/portal_ship.vrx')}
-                resources={[
-                  require('./portal_res/portal_ship/portal_ship_diffuse.png'),
-                  require('./portal_res/portal_ship/portal_ship_normal.png'),
-                  require('./portal_res/portal_ship/portal_ship_specular.png'),
-                ]}
-                type="VRX"
-                physicsBody={{
-                  type: 'Kinematic',
-                  userGravity: true,
-                }}
-              />
-            </ViroPortal>
-            <ViroImage
-              height={0.05}
-              width={0.05}
-              source={require('./portal_res/note.png')}
+        <ViroAmbientLight color="#ffffff" intensity={200} />
+        <ViroPortalScene
+          passable={true}
+          dragType="FixedDistance"
+          onDrag={() => {}}
+        >
+          <ViroPortal position={[0, 0, -1]} scale={[0.1, 0.1, 0.1]}>
+            <Viro3DObject
+              source={require('./portal_res/portal_ship/portal_ship.vrx')}
+              resources={[
+                require('./portal_res/portal_ship/portal_ship_diffuse.png'),
+                require('./portal_res/portal_ship/portal_ship_normal.png'),
+                require('./portal_res/portal_ship/portal_ship_specular.png'),
+              ]}
+              type="VRX"
+              physicsBody={{
+                type: 'Kinematic',
+                userGravity: true,
+              }}
             />
-          </ViroPortalScene>
-        </ViroARPlaneSelector>
+          </ViroPortal>
+          <Viro360Image
+            source={require('../dock.jpg')}
+            // source={require('../NoteOne/tinker.obj')}
+            // resources={[require('../NoteOne/obj.mtl')]}
+            type="OBJ"
+            scale={[0.01, 0.01, 0.01]}
+          />
+        </ViroPortalScene>
+        <ViroAmbientLight color="#ffffff" intensity={200} />
+        <ViroPortalScene
+          passable={true}
+          dragType="FixedDistance"
+          onDrag={() => {}}
+        >
+          <ViroPortal position={[0, -1, -1]} scale={[0.1, 0.1, 0.1]}>
+            <Viro3DObject
+              source={require('./portal_res/portal_ship/portal_ship.vrx')}
+              resources={[
+                require('./portal_res/portal_ship/portal_ship_diffuse.png'),
+                require('./portal_res/portal_ship/portal_ship_normal.png'),
+                require('./portal_res/portal_ship/portal_ship_specular.png'),
+              ]}
+              type="VRX"
+              physicsBody={{
+                type: 'Kinematic',
+                userGravity: true,
+              }}
+            />
+          </ViroPortal>
+          <Viro360Image
+            source={require('../outside.jpg')}
+            type="OBJ"
+            scale={[0.01, 0.01, 0.01]}
+          />
+        </ViroPortalScene>
       </ViroARScene>
     );
   },
