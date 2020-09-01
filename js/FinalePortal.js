a163d67d36446aad8f7cf27711cbf2b7adec717f
Slowgen <kalibobailey@gmail.com>
2020-09-01 16:59:26 -0400

    Feat: Finale portal split off into separate Train Portal

--- js/FinalePortal.js
+++ js/FinalePortal.js
@@ -1,53 +1,26 @@
 'use strict';
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { foundObjectThunk } from '../store/objectState';
+
+
 import {
-  ViroText,
-  ViroAmbientLight,
-  ViroPortal,
-  ViroPortalScene,
-  Viro3DObject,
   ViroNode,
   ViroARTrackingTargets,
   ViroARImageMarker,
 } from 'react-viro';
 
+import TrainPortal from './TrainPortal'
+
 export const FinalePortal = () => {
-  const standardPortalSize = [0.75, 1.5, 0.1]
-  const zeroPortalSize = [0, 0, 0]
 
-  const [ text, setText ] = useState('Click the train to leave')
-  const [ insidePortal, setInsidePortal ] = useState(false)
-  const [ portalPosition, setPortalPosition ] = useState([1, -1, -2])
-  const [ portalSize, setPortalSize ] = useState(zeroPortalSize)
-  const [ portalVisible, setPortalVisible ] = useState(true)
-  const [ keyFound, setKeyFound ] = useState(false)
-
-  const objectFoundStatus = useSelector((state) => state.objectsStatus);
-  const dispatch = useDispatch();
+  const [ text, setText ] = useState('Find a Metrocard in real life, scan it with your phone to escape')
 
   const _onClick = () => {
-    setText('Congratulations! You have escaped! Now get out of here!!!')
-    setPortalPosition([-1, -1, -2])
-    setPortalSize(standardPortalSize)
-    setPortalVisible(true)
+    setText('Find a Metrocard in real life, scan it with your phone to escape')
+    //something ends or resets the game here
   }
 
   const _onAnchorFound = () => {
-    setPortalSize(standardPortalSize)
-  }
-
-  const _onPortalEnter = () => {
-    setInsidePortal(true)
-    setPortalSize(zeroPortalSize)
-  }
-
-  const _onPortalExit = () => {
-    setInsidePortal(false)
-    setPortalVisible(false)
-    setPortalSize(zeroPortalSize)
-    // something that resets the game here
+    // setPortalSize(standardPortalSize)
   }
 
   return (
@@ -56,53 +29,7 @@ export const FinalePortal = () => {
         target={'targetMetrocard'}
         onAnchorFound={_onAnchorFound}
       >
-        <ViroPortalScene
-          passable={true}
-          dragType="FixedDistance"
-          onDrag={() => {}}
-          onPortalEnter={_onPortalEnter}
-          onPortalExit={_onPortalExit}
-        >
-        <ViroAmbientLight color="#ffffff" intensity={500} />
-
-          <ViroPortal
-            position={portalPosition}
-            scale={portalSize}
-            visible={portalVisible}
-          >
-              <Viro3DObject
-                source={require('./res/ARPortals/portal_res/door/portal_archway/portal_archway.vrx')}
-                resources={[
-                  require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_diffuse.png'),
-                  require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_normal.png'),
-                  require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_specular.png'),
-                  require('./res/ARPortals/portal_res/door/portal_archway/portal_entry.png'),
-                ]}
-                type="VRX"
-                visible={portalVisible}
-                // onLoadStart={}
-                // onLoadEnd={}
-              />
-          </ViroPortal>
-      {/* object for the room */}
-          <Viro3DObject
-            source={require('./res/FBXtoVRX/model.vrx')}
-            position={[0.5, -1.8, -1]}
-            scale={[0.015, 0.015, 0.015]}
-            type="VRX"
-            visible={portalVisible}
-            _onClick={_onClick}
-            // onLoadStart={}
-            // onLoadEnd={}
-          />
-
-          <ViroText
-            text={text}
-            scale={[0.5, 0.5, 0.5]}
-            position={[0, 1, -2]}
-          />
-
-        </ViroPortalScene>
+        <TrainPortal />
       </ViroARImageMarker>
     </ViroNode>
   )
--- js/TrainPortal.js
+++ js/TrainPortal.js
@@ -11,9 +11,7 @@ import {
   ViroNode
 } from 'react-viro';
 
-import FinalePortal from './FinalePortal';
-
-export const PortalTwo = () => {
+export const TrainPortal = () => {
   const standardPortalSize = [0.75, 1.5, 0.1]
   const zeroPortalSize = [0, 0, 0]
 
@@ -50,39 +48,38 @@ export const PortalTwo = () => {
   let levelComplete = keyFound && !insidePortal
 
   return (
-    <ViroNode>
       <ViroPortalScene
-        passable={true}
-        dragType="FixedDistance"
-        onDrag={() => {}}
-        onPortalEnter={_onPortalEnter}
-        onPortalExit={_onPortalExit}
-      >
-      <ViroAmbientLight color="#ffffff" intensity={500} />
-      <ViroPortal
-        position={portalPosition}
-        scale={portalSize}
+    passable={true}
+    dragType="FixedDistance"
+    onDrag={() => {}}
+    onPortalEnter={_onPortalEnter}
+    onPortalExit={_onPortalExit}
+  >
+    <ViroAmbientLight color="#ffffff" intensity={500} />
+    <ViroPortal
+      position={portalPosition}
+      scale={portalSize}
+      visible={portalVisible}
+    >
+      <Viro3DObject
+        source={require('./res/ARPortals/portal_res/door/portal_archway/portal_archway.vrx')}
+        resources={[
+          require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_diffuse.png'),
+          require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_normal.png'),
+          require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_specular.png'),
+          require('./res/ARPortals/portal_res/door/portal_archway/portal_entry.png'),
+        ]}
+        type="VRX"
         visible={portalVisible}
-      >
-        <Viro3DObject
-          source={require('./res/ARPortals/portal_res/door/portal_archway/portal_archway.vrx')}
-          resources={[
-            require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_diffuse.png'),
-            require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_normal.png'),
-            require('./res/ARPortals/portal_res/door/portal_archway/portal_archway_specular.png'),
-            require('./res/ARPortals/portal_res/door/portal_archway/portal_entry.png'),
-          ]}
-          type="VRX"
-          visible={portalVisible}
-          // onLoadStart={}
-          // onLoadEnd={}
-        />
+        // onLoadStart={}
+        // onLoadEnd={}
+      />
     </ViroPortal>
     {/* object for the room */}
     <Viro3DObject
-      source={require('./res/FBXtoVRX/beach.vrx')}
-      position={[0.5, -1.8, -1]}
-      scale={[0.015, 0.015, 0.015]}
+      source={require('./res/FBXtoVRX/model.vrx')}
+      // position={[0.5, -1.8, -1]}
+      // scale={[0.015, 0.015, 0.015]}
       type="VRX"
       visible={portalVisible}
       // onLoadStart={}
@@ -94,26 +91,12 @@ export const PortalTwo = () => {
       scale={[0.5, 0.5, 0.5]}
       position={[0, 1, -2]}
     />
-    <Viro3DObject
-      source={require('./res/3dObjects/Key_B_02.obj')}
-      resources={[
-        require('./res/3dObjects/Key_B_02.mtl'),
-        require('./res/3dObjects/keyB_tx.bmp'),
-      ]}
-      type="OBJ"
-      position={[0.8, 0.1, -0.2]}
-      scale={[0.015, 0.015, 0.015]}
-      onClick={_onClick}
-      visible={!keyFound}
-    />
 
       </ViroPortalScene>
 
-      {levelComplete && <FinalePortal />}
-    </ViroNode>
   )
 
 }
 
 
-module.exports = PortalTwo;
+module.exports = TrainPortal;
