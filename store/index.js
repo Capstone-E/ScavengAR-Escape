176b2a400686119301755f8af909d2d2e5f68192
Kristi Nanco <kristiknanco@gmail.com>
2020-09-04 16:56:17 -0400

    Feat: Add portal navigation to store

--- store/index.js
+++ store/index.js
@@ -5,10 +5,12 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 
 import objectsStatus from './objectState'
 import navigator from './navigator'
+import portalNav from './portalNaivigator'
 
 const reducer = combineReducers({
   navigator,
-  objectsStatus
+  objectsStatus,
+  portalNav
 })
 const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, createLogger({collapsed: true})))
 const store = createStore(reducer, middleware)
---
+++ store/portalNaivigator.js
@@ -0,0 +1,35 @@
+// ACTION TYPES
+export const PORTAL_ONE = 'PORTAL_ONE'
+export const PORTAL_TWO = 'PORTAL_TWO'
+export const TRAIN_PORTAL = 'TRAIN_PORTAL'
+export const FINAL_PORTAL = 'FINAL_PORTAL'
+
+// ACTION CREATORS
+export const setPortal = (status, portalName) => ({
+  type: portalName,
+  status
+})
+
+// INITIAL STATE
+const initialState = {
+  portalOne: false,
+  portalTwo: false,
+  finalPortal: false,
+  trainPortal: false
+}
+
+// REDUCER
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case PORTAL_ONE:
+      return {...state, portalOne: action.status}
+    case PORTAL_TWO:
+      return {...state, portalTwo: action.status}
+    case TRAIN_PORTAL:
+      return {...state, trainPortal: action.status}
+    case FINAL_PORTAL:
+      return {...state, finalPortal: action.status}
+    default:
+      return state
+  }
+}
