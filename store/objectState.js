2a71eda4c80d9aa47ec43893f865f3557d864e47
Mona Zheng <mona.a.zheng@gmail.com>
2020-09-07 20:28:39 -0400

    refractor: change to update status of keys depending on specific portal'

--- store/objectState.js
+++ store/objectState.js
@@ -1,11 +1,20 @@
 // action
 const FOUND_OBJECT = 'FOUND_OBJECT'
+const FOUND_KEY_ONE = 'FOUND_KEY_ONE'
+const FOUND_KEY_TWO = 'FOUND_KEY_TWO'
 export const RESET = 'RESET'
 
 // action creator
-export const foundObject = (objectStatus) => ({ // will return with value true if key is collected
-  type: FOUND_OBJECT,
+// will return with value true if key is collected
+export const foundObjectOne = (objectStatus) => ({ 
+  type: FOUND_KEY_ONE,
   objectStatus
+})
+
+// will return with value true if key is collected
+export const foundObjectTwo = (objectStatus) => ({ 
+    type: FOUND_KEY_TWO,
+    objectStatus
 })
 
 export const resetObject = () => ({
@@ -14,13 +23,21 @@ export const resetObject = () => ({
 
 
 // reducer
-const initalState = [] // each value will signify that a key was collected
+// const initalState = [] // each value will signify that a key was collected
+
+// each value will signify that a key was collected
+const initalState = {
+    keyOne: false,
+    keyTwo: false
+}
 
 const reducer = (state = initalState, action) => {
   switch (action.type) {
-    case FOUND_OBJECT:
-      return [...state, action.objectStatus]
-    case RESET:
+    case FOUND_OBJECT_ONE:
+      return {...state, keyOne: action.objectStatus}
+    case FOUND_OBJECT_TWO:
+    return {...state, keyTwo: action.objectStatus}
+    case RESET: // will change later if this works
       return []
     default:
       return state
